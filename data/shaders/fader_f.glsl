697ef8b933db9d08aab7257ecd344fc4e07bd361
Joel Hoover <joelahoover@gmail.com>
2016-03-31 21:56:38 -0400

    Make fragment shader GLSL 1.2 compatable

--- data/shaders/fader_f.glsl
+++ data/shaders/fader_f.glsl
@@ -1,16 +1,15 @@
+//#version 130
+#version 120
 
-
-
-//uniform float hue;
-uniform float saturation = 1.0f;
-uniform float value = 1.0f;
+uniform float saturation = 1.0;
+uniform float value = 1.0;
 
 uniform float time;
 
 // random start
 
 // Bob Jenkins One-At-A-Time hashing algorithm.
-uint hash( uint x ) {
+/*uint hash( uint x ) {
     x += ( x << 10u );
     x ^= ( x >>  6u );
     x += ( x <<  3u );
@@ -39,7 +38,13 @@ float floatConstruct( uint m ) {
 float random( float v ) { return floatConstruct(hash(floatBitsToUint(v))); }
 float random( vec2  v ) { return floatConstruct(hash(floatBitsToUint(v))); }
 float random( vec3  v ) { return floatConstruct(hash(floatBitsToUint(v))); }
-float random( vec4  v ) { return floatConstruct(hash(floatBitsToUint(v))); }
+float random( vec4  v ) { return floatConstruct(hash(floatBitsToUint(v))); }*/
+
+// GLSL 1.2 compatable implementation
+float random(vec3 n)
+{
+  return 0.5 + 0.5 * fract(sin(dot(vec3(n.xy, sin(2.465*n.z)), vec3(12.9898, 78.233, -4.233)))*43758.5453);
+}
 
 // random end
 
